5255f3fef89b68887c9133965b72f3fa2526fd92
이태성 <ga1754@naver.com>
2024-08-26 22:09:36 +0900

    [feat] main header passage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,29 @@ export default function Home() {
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
                 <Image src={snake1} alt="snake1" sizes="width: 100%" />
               </div>
+              <h1
+                className="relative w-fit tracking-tight text-balance
+              mt-16 font-bold !leading-tight text-gray-900 text-5xl
+              md:text-6xl lg:text-7xl"
+              >
+                Your Image on a{" "}
+                <span
+                  className="bg-green-600 px-2
+                text-white"
+                >
+                  Custom
+                </span>{" "}
+                Phone Case
+              </h1>
+              <p
+                className="mt-8 text-lg lg:pr-10 max-w-prose text-center
+              lg:text-left text-balance md:text-wrap"
+              >
+                Capture your favorite memories with your own,{" "}
+                <span className="font-semibold">one-of-one</span> phone case.
+                CaseCobra allows you to protect your memories, not just your
+                phone case.
+              </p>
             </div>
           </div>
         </MaxWidthWrapper>
